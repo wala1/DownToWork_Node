16a13e7700fbb0a06257154935beeda95fba2fcc
wala1 <wala.alimi@esprit.tn>
2023-02-28 23:19:07 +0100

    crud operations for table Topics

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const http = require ('http');
+const mongoose = require('mongoose')
 const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongodbConnection = require('./config/mongoconnection.json');
+
 
 const indexRouter = require('./routes/index');
+const topicsRouter = require('./routes/topic');
 
 const app = express();
-
+//connect to database
+mongoose.connect(
+    mongodbConnection.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("DataBase Connected");
+      })
+      .catch((err) => {
+        console.log(err);
+      }
+);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +37,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/topics', topicsRouter);
 
 //creation du serveur
 const server = http.createServer(app); 
---
+++ models/Topics.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+//create schema
+const topicSchema = new mongoose.Schema({
+    topicName : {
+        type : String,
+        required : true
+    },
+    topicImg : {
+        type : String,
+        required : true
+    }
+})
+//class
+const Topic = mongoose.model('Topic',topicSchema);
+
+exports.Topic = Topic;
+exports.topicSchema = topicSchema;
