4caa19ce11fa3cc8bda16b509d09894f8d045335
Ons <ons.diweni@esprit.tn>
2023-03-08 23:24:46 +0100

    Post crud Operations

--- controllers/postController.js
+++ controllers/postController.js
@@ -37,3 +37,16 @@ exports.delete = (req, res)=>{
 }
 
 
+//**************************** Endpoint to update a Post **************** */
+exports.update = (req, res)=>{
+
+    if(Object.keys(req.body).length === 0){ return res.status(400).send({ message : "post with new informations must be provided"})}
+
+    const id = req.params.id;
+
+    //The { useFindAndModify: false} option is used to avoid using the deprecated findAndModify() method
+    //The { new: true } option tells Mongoose to return the updated document instead of the original one.
+    User.findByIdAndUpdate(id,req.body, { useFindAndModify: false , new: true})
+    .then(post => {(!post) ? res.status(404).send({ message : `Cannot Update post with ${id}. Maybe post not found!`}) :res.send(post)})
+    .catch(err => res.status(500).send({ message : "Error Update user information" , error : err}))
+}
