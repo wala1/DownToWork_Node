2fbb7a5555f9e59ae344fbad4866814609c7644c
wala1 <wala.alimi@esprit.tn>
2023-04-18 19:46:43 +0100

    merge

--- routes/question.js
+++ routes/question.js
@@ -2,15 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { createQuestion, updateQuestion, deleteQuestion, getAllQuestions, getQuestionById, getQuestionsByType, addChoice, updateChoice, deleteChoice, getQuestionsByIdQuiz } = require('../controllers/questionController');
 
-router.post('/', createQuestion);
-router.put('/:id', updateQuestion);
-router.delete('/:id', deleteQuestion);
-router.get('/', getAllQuestions);
-router.get('/:id', getQuestionById);
-router.get('/type/:type', getQuestionsByType);
-router.post('/:id/choices', addChoice);
-router.put('/:id/choices/:choiceId', updateChoice);
-router.delete('/:id/choices/:choiceId', deleteChoice);
 router.get('/', getAllQuestions);
 router.post('/add', createQuestion);
 router.put('/update/:id', updateQuestion);
