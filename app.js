2c15b5d6e33196adf81bc6318e7b7d7f59a2b786
Ons <ons.diweni@esprit.tn>
2023-05-02 01:01:08 +0100

    implementing updateImg endpoint

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ dotenv.config();
   })();
 
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-const { registerUser,LoginUser,GetUser ,forgetPassword , blockUser,unblockUser, findById, update,ChangePassword,verifyCode, desactivateAccount,verifyUser} = require('../controllers/userController.js');
+const { registerUser,LoginUser,GetUser ,forgetPassword , blockUser,unblockUser, findById, update,ChangePassword,verifyCode, desactivateAccount,verifyUser, updateImg} = require('../controllers/userController.js');
 const { protect } = require ('../middleWares/authMiddleWare.js');
 const { validate } = require('../middleWares/validation.js');
 const { signinController, signupController, deleteAccount } = require("../controllers/userController")
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname)
+    }
+  });
+  
+const upload = multer({ storage: storage });
 
 
 router.post('/register',validate,registerUser);
@@ -18,6 +32,8 @@ router.put('/desactivate/:id', desactivateAccount )
 router.get('/block-user/:id' , blockUser);
 router.get('/unblock-user/:id',unblockUser);
 router.post('/delete-account', deleteAccount );
+router.put('/updateImg/:id',upload.single('picture'),updateImg)
+
 
 // router.post("/api/user", async (req, res) => {
 //     const userData = req.body;
