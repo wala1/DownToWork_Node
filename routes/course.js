52794d37ebd96d4c5a903ec8728d1687910eb4de
wala1 <wala.alimi@esprit.tn>
2023-05-10 00:18:58 +0100

    changes

--- routes/course.js
+++ routes/course.js
@@ -2,9 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const router = express.Router();
-const path = require('path');
-
-const { addCourse, courseList , updateCourse ,getCourse ,deleteCourse,downloadCoursePdf ,findCourseByTopicAndLevel,AddCoursePdf,findCourseByType,findCourseByTopic,downloadPdf,uploadFile,test} = require('../controllers/courseController');
+const { addCourse, courseList , updateCourse , getCourse ,deleteCourse , findCourseByType,findCourseByTopic,getALLCourses, downloadPdf,uploadFile} = require('../controllers/courseController');
 
 // Storage engine 
 const storage = multer.diskStorage({
@@ -13,39 +11,26 @@ const storage = multer.diskStorage({
         return cb(null ,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}` )
     }
 })
+
 // Define the multer
 const upload = multer({
     storage : storage,
-})
-//Configuration de multer pour enregistrer les fichiers dans le dossier uploads/
-const storageFile = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  });
-const uploadFileCourse = multer({ 
-    storage: storageFile,
-    dest: 'uploads/' // Spécifiez le dossier de destination pour le fichier téléchargé
-}); 
-
-router.use('/imageCourse', express.static('public/images')); 
+    // limits : {
+    //     fileSize : 10
+    // }
+}) 
+router.use('/courseImage', express.static('public/images')); 
 router.get('/' ,courseList);
+router.post('/add' ,upload.single('courseImage'), addCourse);
 router.get('/find/:id' , getCourse);
+router.put('/update/:id' , updateCourse);
+router.post('/delete/:id' , deleteCourse);
 router.get('/find-type/:type' , findCourseByType);
 router.get('/find-topic/:topicName' , findCourseByTopic);
 router.get('/getCources' ,getALLCourses);
 router.get('/download-course' , downloadPdf);
 router.post('/uploadd',upload.single("asset"),uploadFile)
 
-router.get('/download/:courseId',downloadCoursePdf);
-router.get('/find-topic-level/:topicName/:level',findCourseByTopicAndLevel);
-router.put('/update/:id' , updateCourse);
-router.post('/add' ,upload.single('imageCourse'), addCourse);
-router.post('/add-pdf',uploadFileCourse.single('pdf') ,AddCoursePdf);
-router.delete('/delete/:id' , deleteCourse);
 
 
 module.exports = router;
